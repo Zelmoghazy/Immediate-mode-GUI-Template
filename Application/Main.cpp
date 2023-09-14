eca4891695698902ab32e1a8e5667e52fe47d3fe
zelmoghazy <zelmoghazy@gmail.com>
2023-09-14 16:50:57 +0300

    Image Class

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -7,43 +7,11 @@
 #include "imgui_impl_opengl3.h"
 #include "Application.h"
 #include "Style.h"
+#include "Image.h"
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
 
-// Simple helper function to load an image into a OpenGL texture with common settings
-bool LoadTextureFromFile(const char* filename, GLuint* out_texture, int* out_width, int* out_height)
+int main(void) 
 {
-    // Load from file
-    int image_width = 0;
-    int image_height = 0;
-    unsigned char* image_data = stbi_load(filename, &image_width, &image_height, NULL, 4);
-    if (image_data == NULL)
-        return false;
-
-    // Create a OpenGL texture identifier
-    GLuint image_texture;
-    glGenTextures(1, &image_texture);
-    glBindTexture(GL_TEXTURE_2D, image_texture);
-
-    // Setup filtering parameters for display
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // This is required on WebGL for non power-of-two textures
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Same
-
-    // Upload pixels into texture
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image_width, image_height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
-    stbi_image_free(image_data);
-
-    *out_texture = image_texture;
-    *out_width = image_width;
-    *out_height = image_height;
-
-    return true;
-}
-
-int main() {
     // Initialize GLFW
     if (!glfwInit()) {
         return -1;
@@ -66,11 +34,8 @@ int main() {
         return -1;
     }
 
-    int my_image_width = 0;
-    int my_image_height = 0;
-    GLuint my_image_texture = 0;
-    bool ret = LoadTextureFromFile("assets/pic1.jpg", &my_image_texture, &my_image_width, &my_image_height);
-    IM_ASSERT(ret);
+        /* Load Images */
+    Image img("assets/pic1.jpg");
 
     // Setup ImGui context
     IMGUI_CHECKVERSION();
@@ -118,11 +83,11 @@ int main() {
          * Loading an Image 
          */
         ImGui::Begin("OpenGL Texture");
-        ImGui::Text("pointer = %p", my_image_texture);
-        ImGui::Text("size = %d x %d", my_image_width, my_image_height);
-        ImGui::SliderInt("Width", &my_image_width, 1, 1000);
-        ImGui::SliderInt("Height", &my_image_height, 1, 1000);
-        ImGui::Image((void*)(intptr_t)my_image_texture, ImVec2(my_image_width, my_image_height));
+        ImGui::Text("pointer = %p", img.image_texture);
+        ImGui::Text("size = %d x %d", img.width, img.height);
+        ImGui::SliderInt("Width", &img.width, 1, 1000);
+        ImGui::SliderInt("Height", &img.height, 1, 1000);
+        ImGui::Image((void*)(intptr_t)img.image_texture, ImVec2(img.width, img.height));
         ImGui::End();
         /* *************************************************************************************** */
 
---
+++ Application/include/Image.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "glew.h"
+
+
+class Image
+{
+public:
+    GLuint image_texture;
+    int width;
+    int height;
+
+    Image(const char* filename);
+};
